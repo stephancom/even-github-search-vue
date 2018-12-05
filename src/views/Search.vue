57a53aa64e210bce02ded3db5ba8b4bb3c51179c
stephan.com <stephan@stephan.com>
2018-12-05 04:25:21 -0500

    rough form

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,10 +1,49 @@
 <template lang="pug">
-  .search
-    h1 Even Financial GitHub Repository Search
+  b-container.search
+    b-row
+      b-col
+        h1 Even Financial GitHub Repository Search
+    b-form(@submit="onSubmit" @reset="onReset")
+      b-form-row
+        b-col
+          b-form-input(id='text' type='text' v-model='form.q' placeholder='react')
+        b-col
+          b-form-input(id='stars' type='text' v-model='form.stars' placeholder='>100')
+      b-form-row
+        b-col
+          b-form-select(id='license' :options='licenses' v-model='form.license')
+        b-col
+          b-form-checkbox(id='forked' v-model='form.forked')
+      b-form-row
+        b-col
+          b-button(type='submit' variant='primary') Search
+
+    hr
+    b-row
+      b-col Please enter query and click SEARCH above, results appear here
 </template>
 
 <script>
 export default {
-  name: "search"
+  name: "search",
+  data () {
+    return {
+      form: {
+        text: '',
+        stars: '',
+        license: null,
+        forked: []
+      },
+      licenses: ['', 'MIT', 'ISC', 'Apache', 'GPL']
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      // TODO
+    },
+    onReset (evt) {
+      // TODO
+    }
+  }
 };
 </script>
