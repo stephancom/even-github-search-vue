4cd079f6dd4bd3f1ebd8af8fb76e54d33e885244
stephan.com <stephan@stephan.com>
2018-12-05 12:41:09 -0500

    form labels, layout

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,22 +1,24 @@
 <template lang="pug">
-  b-container.search
+  .search
     b-row
       b-col
-        h1 Even Financial GitHub Repository Search
-    b-form(@submit="onSubmit" @reset="onReset")
-      b-form-row
-        b-col
-          b-form-input(id='text' type='text' v-model='form.q' placeholder='react')
-        b-col
-          b-form-input(id='stars' type='text' v-model='form.stars' placeholder='>100')
-      b-form-row
-        b-col
-          b-form-select(id='license' :options='licenses' v-model='form.license')
-        b-col
-          b-form-checkbox(id='forked' v-model='form.forked')
-      b-form-row
-        b-col
-          b-button(type='submit' variant='primary') Search
+        h1.heading Even Financial GitHub Repository Search
+    b-row
+      b-col
+        b-form(@submit="onSubmit" @reset="onReset")
+          b-row
+            b-form-group.col(label='Text' label-for='text')
+              b-form-input(id='text' type='text' v-model='form.q' placeholder='react')
+            b-form-group.col(label='Stars' label-for='stars')
+              b-form-input(id='stars' type='text' v-model='form.stars' placeholder='>100')
+          b-row
+            b-form-group.col(label='License' label-for='license')
+              b-form-select(id='license' :options='licenses' v-model='form.license')
+            b-form-group.col
+              b-form-checkbox(id='forked' v-model='form.forked') Include Forked
+          b-row
+            b-col.footer
+              b-button(type='submit' variant='primary') Search
 
     hr
     b-row
@@ -47,3 +49,14 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="sass">
+  .search
+    h1.heading, .footer
+      padding: 2em 0
+      text-align: center
+    form 
+      .form-group
+        padding-left: 5em
+        padding-right: 5em
+</style>
